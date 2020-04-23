2804b8f42e984a3f3e689b85993d4f8f20081cc4
chenzhenghan <841400592@qq.com>
2020-04-23 20:33:08 +0800

    回归基本的打包

--- src/index.js
+++ src/index.js
@@ -1,47 +1,13 @@
-import _ from "lodash";
-import "./style.css";
-import Icon from "./5.jpeg";
-import Data from "./data.xml";
-
 function component() {
   let element = document.createElement("div");
 
-  element.innerHTML = _.join(
-    [
-      "说金钱是罪恶，都在捞;说美女是祸水，都想要;说高处不胜寒，都在爬;说烟酒伤身体，都不戒;说天堂最美好，都不去!",
-      "字体库字体",
-    ],
-    " "
-  );
+  element.innerHTML = "测试webpack知识点";
   element.addEventListener("click", () => {
     alert("clicking");
   });
   element.classList.add("hello");
 
-  // 将图像添加到我们已经存在的 div 中。
-  var myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
-
   return element;
 }
 
 document.body.appendChild(component());
-
-function component2() {
-  let element = document.createElement("div");
-  element.innerHTML =
-    "说金钱是罪恶，都在捞;说美女是祸水，都想要;说高处不胜寒，都在爬;说烟酒伤身体，都不戒;说天堂最美好，都不去! 普通字体";
-  return element;
-}
-
-document.body.appendChild(component2());
-console.log(Data);
-
-window.addEventListener("beforeunload", (event) => {
-  debugger;
-  // Cancel the event as stated by the standard.
-  event.preventDefault();
-  // Chrome requires returnValue to be set.
-  event.returnValue = "";
-});
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,5 @@
-// webpack.config.js 是运行在 Node.js 环境中的代码，所以直接可以使用 path 之类的 Node.js 内置模块。
 const path = require("path");
 
-// 一定记得运行 Webpack 前先注释掉这里。
-// import { Configuration } from "webpack";
-
-/**
- * @type {Configuration}
- */
 const config = {
   entry: "./src/index.js",
   output: {
@@ -14,31 +7,6 @@ const config = {
     path: path.join(__dirname, "dist"),
   },
   mode: "none", // https://webpack.js.org/configuration/mode/ development | production | none
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-        // webpack 根据正则表达式，来确定应该查找哪些文件，并将其提供给指定的 loader。在这个示例中，所有以 .css 结尾的文件，都将被提供给 style-loader 和 css-loader。
-      },
-      {
-        test: /\.(png|svg|jpg|gif|jpeg)$/,
-        use: ["file-loader"],
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ["file-loader"],
-      },
-      {
-        test: /\.(csv|tsv)$/,
-        use: ["csv-loader"],
-      },
-      {
-        test: /\.xml$/,
-        use: ["xml-loader"],
-      },
-    ],
-  },
 };
 
 module.exports = config;
